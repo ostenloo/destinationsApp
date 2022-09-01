b79f75921800506ef47d6a097cc75b58d6383c4d
Austin Liu <2ea64fad26@gmail.com>
2022-09-01 09:33:49 -0500

    Merge branch 'main' of https://github.com/ostenloo/destinationsApp

--- pages/index.js
+++ pages/index.js
@@ -12,14 +12,14 @@ export default function Home() {
     <Head> 
         <title> Destinations </title> 
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@500&family=Yellowtail&display=swap" rel="stylesheet" />
     </Head> 
     <div> 
       <Navbar /> 
       <div className = {styles.main} > 
         { Destinations.map( (destination,index) => (
-          <div className = {`${styles.card}`} key = {index}>
+          <div className = {`${styles.card}`}>
             <DestinationCard {...destination} /> 
           </div> 
         ))}
