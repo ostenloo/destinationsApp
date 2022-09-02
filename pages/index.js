6d7761b5870762c5e94eb710ae575252a89e8edb
Austin Liu <2ea64fad26@gmail.com>
2022-09-01 20:40:55 -0500

    added _document.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,6 @@ export default function Home() {
     <>
     <Head> 
         <title> Destinations </title> 
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-        <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@500&family=Yellowtail&display=swap" rel="stylesheet" />
     </Head> 
     <div> 
       <Navbar /> 
